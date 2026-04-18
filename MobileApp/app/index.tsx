ef115092c82c3d7cf2716cfd49fda2c443686ae7
AdamNajajreh <adamnajajreh@gmail.com>
2026-04-18 00:45:42 -0400

    mobile app set up

---
+++ MobileApp/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="text-xl font-bold text-gray-900">Campus Navigator</Text>
+    </View>
+  );
+}
